e2ab3551ea5a537490272f994f96b242779be1f2
ClausHofman <46158@edu.raseko.fi>
2022-04-20 11:05:26 +0300

    extend main template

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -7,7 +7,7 @@ def home(request): #1
     return render(request, 'accounts/dashboard.html') #2
 
 def products(request): #1
-    return HttpResponse('tuotesivu') #1
+    return render(request, 'accounts/products.html') #2
 
 def customer(request): #1
-    return HttpResponse('customer') #1
+    return render(request, 'accounts/customer.html') #2
