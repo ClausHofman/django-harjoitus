908b74df53d12183c2d844af88ff251a8268ed0a
ClausHofman <46158@edu.raseko.fi>
2022-04-21 13:37:41 +0300

    create string value function

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -7,5 +7,7 @@ class Customer(models.Model):
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-
 # after creating the model, quit server and run python manage.py makemigrations
+
+    def __str__(self):
+        return self.name
