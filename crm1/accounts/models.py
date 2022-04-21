369adc718b0253593cdf10fe381239b22c69d192
ClausHofman <46158@edu.raseko.fi>
2022-04-21 11:23:54 +0300

    create model, run makemigrations

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
 # Create your models here.
+# Python classes that inherit from django models, create classes that represent database tables
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True) # null True allows to make changes to database, if we import data without a name don't get error, without it will get a default value error
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+# after creating the model, quit server and run python manage.py makemigrations
