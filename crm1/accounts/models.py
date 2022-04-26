5d73f00f43161c34b6911c5f3a026ceed68f8c99
ClausHofman <46158@edu.raseko.fi>
2022-04-26 09:59:10 +0300

    blank and string value

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -14,6 +14,8 @@ class Customer(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=200, null=True)
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     CATEGORY = (
@@ -24,13 +26,14 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)   # blank don't need description
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     tags = models.ManyToManyField(Tag)
 
 
     def __str__(self):
         return self.name
+
 class Order(models.Model):
     STATUS = (
             ('Pending', 'Pending'),
