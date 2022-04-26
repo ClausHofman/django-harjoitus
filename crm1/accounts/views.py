26b3acd22a6667c7a382572f7a30ba75003ce72d
ClausHofman <46158@edu.raseko.fi>
2022-04-26 10:56:27 +0300

    import for querying

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse #1
-
 # Create your views here.
+from .models import *
+
 
 def home(request): #1
     return render(request, 'accounts/dashboard.html') #2
