cb6ed6f8f7b6eb478beff9074ff5f550715fb81a
ClausHofman <46158@edu.raseko.fi>
2022-04-28 22:38:21 +0300

    name paths

--- crm1/accounts/urls.py
+++ crm1/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path #1
 from . import views #1
 
+# give names to pages
 
 urlpatterns = [
-    path('', views.home), #1 find url path and trigger function in views.py
-    path('products/', views.products), #1
-    path('customer/<str:pk_test>/', views.customer), # use angle brackets, add <str:pk_test>/, pk_test has to be in the customer variable in view.py
+    path('', views.home, name="home"), # find url path and trigger function in views.py
+    path('products/', views.products, name="products"),
+    path('customer/<str:pk_test>/', views.customer,name="customer"), # use angle brackets, add <str:pk_test>/, pk_test has to be in the customer variable in view.py
 ]
