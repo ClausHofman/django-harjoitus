b5db1358bedd02ee9f8d14e94ed78ddc0b93e3dc
ClausHofman <46158@edu.raseko.fi>
2022-04-28 18:03:32 +0300

    dynamic path

--- crm1/accounts/urls.py
+++ crm1/accounts/urls.py
@@ -5,5 +5,5 @@ from . import views #1
 urlpatterns = [
     path('', views.home), #1 find url path and trigger function in views.py
     path('products/', views.products), #1
-    path('customer/', views.customer), #1
+    path('customer/<str:pk_test>/', views.customer), # use angle brackets, add <str:pk_test>/, pk_test has to be in the customer variable in view.py
 ]
