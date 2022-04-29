c9e066497da6ac9f18b65fb0bd955e529e48d514
ClausHofman <46158@edu.raseko.fi>
2022-04-29 18:28:47 +0300

    delete function

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -70,5 +70,9 @@ def updateOrder(request, pk):
 
 def deleteOrder(request, pk):
     order = Order.objects.get(id=pk)
+    if request.method == "POST":
+        order.delete()
+        return redirect('/')
+
     context = {'item':order}
     return render(request, 'accounts/delete.html', context)
